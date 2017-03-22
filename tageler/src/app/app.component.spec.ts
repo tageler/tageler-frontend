5d9e52de8fa32afa69d37b5cf2bf9d8e887a0b7d
Manning94 <sven.schmid@bluewin.ch>
2017-03-22 15:28:21 +0100

    added module for testing

--- tageler/src/app/app.component.spec.ts
+++ tageler/src/app/app.component.spec.ts
@@ -1,6 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
+import {
+  RouterTestingModule
+} from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +12,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [ RouterTestingModule ]
     }).compileComponents();
   }));
 
