b6a499266491fd3fbfefff734724ffeee866ec1d
Manning94 <sven.schmid@bluewin.ch>
2017-04-11 14:23:41 +0200

    deleted modules

--- tageler/node_modules/ng-lightning/buttons/button-icon.d.ts
+++
@@ -1,9 +0,0 @@
-import { ElementRef, Renderer } from '@angular/core';
-export declare class NglButtonIcon {
-    element: ElementRef;
-    renderer: Renderer;
-    private _type;
-    nglButtonIcon: 'container' | 'border' | 'border-filled' | 'small' | '';
-    constructor(element: ElementRef, renderer: Renderer);
-    private normalize(type?);
-}
--- tageler/node_modules/ng-lightning/pills/pill-image.d.ts
+++
@@ -1,7 +0,0 @@
-import { ElementRef, Renderer } from '@angular/core';
-export declare class NglPillImage {
-    private element;
-    private renderer;
-    constructor(element: ElementRef, renderer: Renderer);
-    ngAfterContentInit(): void;
-}
