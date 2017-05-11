1f542a55e0d7772a2d9756a4b8607ac1cc31161f
Ramona <ramona.beck@bluewin.ch>
2017-05-11 22:00:46 +0200

    Updated tageler model

--- src/app/tagelers/tageler.ts
+++ src/app/tagelers/tageler.ts
@@ -1,21 +1,24 @@
 export class Tageler {
   _id?: string;
   title: string;
-  text?: string;
+  text: string;
   group: [string];
   start: Date;
   end: Date;
-  bringAlong: string;
-  uniform: string;
+  bringAlong?: string;
+  uniform?: string;
   picture?: string;
   checkout?: {
-    deadline?: Date;
+    deadline: Date;
     contact?: [{
-      name?: string;
+      name: string;
       phone?: string;
       mail?: string;
       other?: string;
     }]
-  }
+  };
   free: boolean;
+  background_color?: string;
+  color?: string;
+  font_family?: string;
 }
