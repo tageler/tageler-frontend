890efeae754f7ff71e4cfa6047ebd56f1da0a53e
Ramona <ramona.beck@bluewin.ch>
2017-05-22 09:00:24 +0200

    Try to fix time display so american time format works too

--- src/app/pipes/toLocalTimePipe.pipe.ts
+++ src/app/pipes/toLocalTimePipe.pipe.ts
@@ -16,18 +16,61 @@ export class ToLocalTimePipe implements PipeTransform {
   transform(date: Date) {
     let parsedDateTime;
 
+    console.log(date)
+    console.log(new Date(date))
+    console.log(new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString());
+    console.log(new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().indexOf(':'))
+    console.log(new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().charAt(2));
+    console.log(new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().charAt(2) == ':');
+
+
+
     if (date==null) {
       return null;
     }
 
     // Winter time
     if (new Date(date).getTimezoneOffset() == -60) {
-      parsedDateTime = new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().slice(0, 5);
+
+      // european format, e.g. 05:00:00
+      if (new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().charAt(2) == ':') {
+        parsedDateTime = new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().slice(0, 5);
+      }
+
+      // american format, e.g. 5:00:00
+      if (new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().charAt(1) == ':') {
+        parsedDateTime = new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().slice(0, 4);
+
+        if (new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().includes('PM')) {
+          parsedDateTime += ' PM'
+        }
+
+        if (new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().includes('AM')) {
+          parsedDateTime += ' PM'
+        }
+      }
     }
 
     // Summer time
     if (new Date(date).getTimezoneOffset() == -120) {
-      parsedDateTime = new Date(new Date(date).setHours(new Date(date).getHours() - 2)).toLocaleTimeString().slice(0, 5);
+
+      // european format, e.g. 05:00:00
+      if (new Date(new Date(date).setHours(new Date(date).getHours() - 2)).toLocaleTimeString().charAt(2) == ':') {
+        parsedDateTime = new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().slice(0, 5);
+      }
+
+      // american format, e.g. 5:00:00
+      if (new Date(new Date(date).setHours(new Date(date).getHours() - 2)).toLocaleTimeString().charAt(1) == ':') {
+        parsedDateTime = new Date(new Date(date).setHours(new Date(date).getHours() - 1)).toLocaleTimeString().slice(0, 4);
+
+        if (new Date(new Date(date).setHours(new Date(date).getHours() - 2)).toLocaleTimeString().includes('PM')) {
+          parsedDateTime += ' PM'
+        }
+
+        if (new Date(new Date(date).setHours(new Date(date).getHours() - 2)).toLocaleTimeString().includes('AM')) {
+          parsedDateTime += ' PM'
+        }
+      }
     }
 
     return parsedDateTime;
